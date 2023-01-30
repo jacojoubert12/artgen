e071b417135ba62f12858ba5aa1c07931c0e9d58
WilloBee12 <carmen@enginosoft.com>
2023-01-30 20:32:57 +0200

    SubscriptionView editing

--- lib/views/main_detail_views/subscription_view.dart
+++ lib/views/main_detail_views/subscription_view.dart
@@ -1,87 +1,240 @@
+import 'package:artgen/responsive.dart';
 import 'package:flutter/material.dart';
 
 class SubscriptionView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 200,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
-                  fit: BoxFit.cover)),
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20))),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(112, 181, 5, 134),
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
+                      fit: BoxFit.cover)),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                decoration:
+                    BoxDecoration(color: Color.fromARGB(112, 181, 5, 134)),
+                child: Responsive.isMobile(context)
+                    ? Column(children: getSubscriptions())
+                    : Row(children: getSubscriptions()),
+              ),
+            ),
+          ],
         ),
-        Container(
-          decoration: BoxDecoration(color: Color.fromARGB(112, 181, 5, 134)),
-          child: Row(
-            children: <Widget>[
-              SizedBox(),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  constraints: BoxConstraints(minWidth: 100, maxWidth: 200),
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(35)),
-                      color: Color.fromARGB(255, 31, 30, 30)),
-                  child: Column(
-                    children: <Widget>[
-                      Text('Starter', style: TextStyle(color: Colors.white)),
-                      SizedBox(height: 400),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text('Subscribe'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  constraints: BoxConstraints(minWidth: 100, maxWidth: 200),
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(35)),
-                      color: Color.fromARGB(255, 31, 30, 30)),
-                  child: Column(
-                    children: <Widget>[
-                      Text('Pro', style: TextStyle(color: Colors.white)),
-                      SizedBox(
-                        height: 400,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text('Subscribe'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  constraints: BoxConstraints(minWidth: 100, maxWidth: 200),
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(35)),
-                      color: Color.fromARGB(255, 31, 30, 30)),
-                  child: Column(
-                    children: <Widget>[
-                      Text('Max', style: TextStyle(color: Colors.white)),
-                      SizedBox(height: 400),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text('Subscribe'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
+      ),
     );
+  }
+
+  List<Widget> getSubscriptions() {
+    return [
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(35)),
+            color: Color.fromARGB(255, 31, 30, 30)),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Starter',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 20.0, // text size
+                color: Color.fromARGB(255, 255, 251, 251), // text color
+              ),
+            ),
+            Text(
+              'Monthly Plan',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 15.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '''
+                -No Adds
+                -1000 Images Per month
+                -Share Image on Social Media
+                -Public Image
+                -Moderate Speed and Quality
+                -Prompt Length 50 Words
+               -Not safe for work images can not be viewed
+                -Batch Generation - Batches of 10 images 
+                -Model Style Selection
+                Watermark on image
+              ''',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 15.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            Text(
+              '\$8',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 30.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Subscribe'),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(35)),
+            color: Color.fromARGB(255, 31, 30, 30)),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Pro',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 20.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            Text(
+              'Monthly Plan',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 15.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '''
+                -No Adds
+                -4000 Images Per month
+                -Share Image on Social Media
+                -Public Image
+                -Fast Speed and Good Quality
+                -Extra Prompt words,  Length of 100 Words
+                -See Prompt of chosen images
+                -Not safe for work images can not be viewed
+                -All Settings Available
+                -Batch Generation - Batches of 100 images 
+                -Model Style Selection
+                -No Watermark on image
+              ''',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 15.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            Text(
+              '\$25',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 30.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Subscribe'),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(35)),
+            color: Color.fromARGB(255, 31, 30, 30)),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Max',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 20.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            Text(
+              'Monthly Plan',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 15.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '''
+                -No Adds
+                -4000 Images Per month
+                -Share Image on Social Media
+                -Public Image
+                -Fast Speed and Good Quality
+                -Extra Prompt words,  Length of 100 Words
+                -See Prompt of chosen images
+                -Not safe for work images can not be viewed
+                -All Settings Available
+                -Batch Generation - Batches of 100 images 
+                -Model Style Selection
+                -No Watermark on image
+              ''',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 15.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            Text(
+              '\$25',
+              style: TextStyle(
+                fontFamily:
+                    'custom font', // remove this if don't have custom font
+                fontSize: 30.0, // text size
+                color: Color.fromARGB(255, 255, 255, 255), // text color
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Subscribe'),
+            ),
+          ],
+        ),
+      ),
+    ];
   }
 }
