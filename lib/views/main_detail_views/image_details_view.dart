38e2586ad12c1bd6f979afc7ef6377b25ec45924
Jaco Joubert <jacojoubert12@gmail.com>
2023-02-11 14:51:42 +0200

    push

--- lib/views/main_detail_views/image_details_view.dart
+++ lib/views/main_detail_views/image_details_view.dart
@@ -18,10 +18,10 @@ class _ImageDetailsModalState extends State<ImageDetailsModal> {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        width: Responsive.isDesktop(context)
-            ? MediaQuery.of(context).size.width * 0.6
-            : MediaQuery.of(context).size.width * 0.95,
-        height: MediaQuery.of(context).size.height * 0.95,
+        // width: Responsive.isDesktop(context)
+        //     ? MediaQuery.of(context).size.width * 0.8
+        //     : MediaQuery.of(context).size.width * 0.95,
+        // height: MediaQuery.of(context).size.height * 0.95,
         decoration: BoxDecoration(
             color: kBgDarkColor,
             borderRadius: BorderRadius.only(
@@ -56,235 +56,181 @@ class _ImageDetailsModalState extends State<ImageDetailsModal> {
             ),
             SizedBox(height: kDefaultPadding),
             Expanded(
-              flex: 5,
               child: Stack(
                 children: [
                   Image.network(
                     widget.selectedImageUrl,
                     // width: 400,
                     // height: 400,
-                    fit: BoxFit.cover,
+                    fit: BoxFit.contain,
                   ),
                 ],
               ),
             ),
+            Row(
+              children: [
+                Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(colors: [
+                        Color.fromARGB(255, 61, 2, 50),
+                        Color.fromARGB(255, 10, 6, 20)
+                      ])),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Share.share(
+                          'check out the Image I have generated with Art Gen https://google.com');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    child: Text('Share'),
+                  ),
+                ),
+                // SizedBox(
+                //   child: Text('Safe Search'),
+                // ),
+                // LiteRollingSwitch(
+                //   //initial value
+                //   value: false,
+                //   textOn: 'Search Safe',
+                //   textOff: 'Unsafe',
+                //   colorOn: Colors.greenAccent[700]!,
+                //   colorOff: Colors.redAccent[700]!,
+                //   iconOn: Icons.done,
+                //   iconOff: Icons.remove_circle_outline,
+                //   textSize: 10.0,
 
-            // 2 Buttons next to each other
-            Expanded(
-              child: Row(
-                children: [
-                  Container(
-                    height: 50,
-                    width: 100,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(colors: [
-                          Color.fromARGB(255, 61, 2, 50),
-                          Color.fromARGB(255, 10, 6, 20)
-                        ])),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Share.share(
-                            'check out the Image I have generated with Art Gen https://google.com');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                      child: Text('Share'),
+                //   onSwipe: (bool state) {}, onChanged: (bool) {},
+                //   onTap: () {}, onDoubleTap: () {},
+                // ),
+                // Container(
+                //   height: 35,
+                //   width: 100,
+                //   decoration: BoxDecoration(
+                //       borderRadius: BorderRadius.circular(10),
+                //       gradient: LinearGradient(colors: [
+                //         Color.fromARGB(255, 61, 2, 50),
+                //         Color.fromARGB(255, 10, 6, 20)
+                //       ])),
+                //   child: ElevatedButton(
+                //     style: ElevatedButton.styleFrom(
+                //       backgroundColor: Colors.transparent,
+                //       shadowColor: Colors.transparent,
+                //       shape: RoundedRectangleBorder(
+                //           borderRadius: BorderRadius.circular(10)),
+                //     ),
+                //     child: Text('Enhance'),
+                //     onPressed: () {},
+                //   ),
+                // ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(colors: [
+                        Color.fromARGB(255, 61, 2, 50),
+                        Color.fromARGB(255, 10, 6, 20)
+                      ])),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
                     ),
+                    child: Text('Rerun'),
+                    onPressed: () {},
                   ),
-                  SizedBox(
-                    child: Text('Safe Search'),
-                  ),
-                  LiteRollingSwitch(
-                    //initial value
-                    value: false,
-                    textOn: 'Yes',
-                    textOff: 'No',
-                    colorOn: Colors.greenAccent[700]!,
-                    colorOff: Colors.redAccent[700]!,
-                    iconOn: Icons.done,
-                    iconOff: Icons.remove_circle_outline,
-                    textSize: 10.0,
-
-                    onSwipe: (bool state) {}, onChanged: (bool) {},
-                    onTap: () {}, onDoubleTap: () {},
-                    // onChanged: (bool state) {
-                    //   //Use it to manage the different states
-                    //   print('Current State of SWITCH IS: $state');
-                    // },
-                  ),
-                  // Expanded(
-                  //   child: RoundedButton(
-                  //     press: () {},
-                  //     text: 'Button 2',
-                  //   ),
-                  // ),
-                  // SizedBox(width: kDefaultPadding),
-                  // SizedBox(width: kDefaultPadding),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    height: 35.0,
-                    width: 350,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(colors: [
-                          Color.fromARGB(255, 61, 2, 50),
-                          Color.fromARGB(255, 10, 6, 20)
-                        ])),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                      child: Text('Enhance'),
-                      onPressed: () {},
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(colors: [
+                        Color.fromARGB(255, 61, 2, 50),
+                        Color.fromARGB(255, 10, 6, 20)
+                      ])),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
                     ),
+                    child: Text('Remix'),
+                    onPressed: () {},
                   ),
-                ],
-              ),
-            ),
-
-            Expanded(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    height: 35.0,
-                    width: 155,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(colors: [
-                          Color.fromARGB(255, 61, 2, 50),
-                          Color.fromARGB(255, 10, 6, 20)
-                        ])),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                      child: Text('Rerun'),
-                      onPressed: () {},
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(colors: [
+                        Color.fromARGB(255, 61, 2, 50),
+                        Color.fromARGB(255, 10, 6, 20)
+                      ])),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
                     ),
+                    child: Text('Reimage'),
+                    onPressed: () {},
                   ),
-                  Container(
-                    height: 35.0,
-                    width: 155,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(colors: [
-                          Color.fromARGB(255, 61, 2, 50),
-                          Color.fromARGB(255, 10, 6, 20)
-                        ])),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                      child: Text('Remix'),
-                      onPressed: () {},
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(colors: [
+                        Color.fromARGB(255, 61, 2, 50),
+                        Color.fromARGB(255, 10, 6, 20)
+                      ])),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
                     ),
+                    child: Text('Make Privte'),
+                    onPressed: () {},
                   ),
-                  Container(
-                    height: 35.0,
-                    width: 155,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(colors: [
-                          Color.fromARGB(255, 61, 2, 50),
-                          Color.fromARGB(255, 10, 6, 20)
-                        ])),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                      child: Text('Reimage'),
-                      onPressed: () {},
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(colors: [
+                        Color.fromARGB(255, 61, 2, 50),
+                        Color.fromARGB(255, 10, 6, 20)
+                      ])),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
                     ),
+                    child: Text('Delete'),
+                    onPressed: () {},
                   ),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                children: [
-                  SizedBox(height: kDefaultPadding),
-                  Text('Text View 1'),
-                  SizedBox(height: kDefaultPadding),
-                  // Text('Text View 2'),
-                  // SizedBox(height: kDefaultPadding),
-                  // Text('Text View 3'),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    height: 35.0,
-                    width: 200,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(colors: [
-                          Color.fromARGB(255, 61, 2, 50),
-                          Color.fromARGB(255, 10, 6, 20)
-                        ])),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                      child: Text('Make Privte'),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Container(
-                    height: 35.0,
-                    width: 200,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(colors: [
-                          Color.fromARGB(255, 61, 2, 50),
-                          Color.fromARGB(255, 10, 6, 20)
-                        ])),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                      child: Text('Delete'),
-                      onPressed: () {},
-                    ),
-                  ),
-                ],
-              ),
-            ),
+                ),
+              ],
+            )
           ],
         ),
       ),
