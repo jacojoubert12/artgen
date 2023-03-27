b072e5202a99d5d6ee8e9e0d06bc401eea2ef65e
Walter Heymans <walterheymans07@gmail.com>
2023-03-27 08:04:08 +0200

    lib: file_picker: Use different upload functions based on platform
    Split the uploadFile functions for mobile and web platforms. This is
    required because of unsupported libraries that will be imported on
    mobile platforms, used only for web.
    
    Signed-off-by: Walter Heymans <walterheymans07@gmail.com>

---
+++ lib/components/file_picker_web.dart
@@ -0,0 +1,46 @@
+import 'dart:typed_data';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+Future<String?> uploadFile(BuildContext context, FirebaseStorage storage) async {
+  String filename = "";
+  final result = await FilePickerWeb.platform
+      .pickFiles(allowMultiple: false, type: FileType.image);
+
+  if (result == null) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("No file was selected"),
+      ),
+    );
+    return null;
+  } else {
+    Uint8List uploadFile = result.files.single.bytes!;
+    filename = result.files.single.name;
+    if (uploadFile.length / 1000.0 / 1000.0 > 3) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Upload file too large. Max size = 3MB"),
+        ),
+      );
+      print("upload size too large");
+      return null;
+    }
+
+    try {
+      await storage
+          .ref('uploads/$filename')
+          .putData(uploadFile, SettableMetadata(contentType: 'image/png'));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Upload completed")),
+      );
+      print("Upload done");
+    } on FirebaseException catch (e) {
+      print(e);
+    }
+  }
+
+  return await storage.ref('uploads/$filename').getDownloadURL();
+}
