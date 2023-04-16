ad1954576f3e85dd94c893ee58dad83dfc02b221
Carmen Joubert <ckbottermot@gmail.com>
2023-04-16 22:58:50 +0200

    started adding revenuecat

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:universal_platform/universal_platform.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:artgen/auth_gate.dart';
-import 'package:artgen/constants.dart';
 import 'package:artgen/views/main/main_view.dart';
-import 'package:artgen/views/Welcome/welcome_screen.dart';
 import 'package:artgen/components/my_input_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'firebase_options.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +14,15 @@ Future<void> main() async {
 
   if (!kIsWeb && (UniversalPlatform.isAndroid || UniversalPlatform.isIOS)) {
     await MobileAds.instance.initialize();
+  }
+
+  PurchasesConfiguration configuration;
+  if (UniversalPlatform.isAndroid) {
+    configuration = PurchasesConfiguration("goog_oqfbiGbdvDEhZRJERiWJjtIJWzA");
+    await Purchases.configure(configuration);
+  } else if (UniversalPlatform.isIOS) {
+    // configuration = PurchasesConfiguration(<public_ios_sdk_key>);
+    // await Purchases.configure(configuration);
   }
 
   runApp(MyApp());
